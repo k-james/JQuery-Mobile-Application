5c0721d17386a5dedc3f6b52e05691e72d009a01
k_james <katelin.james@gmail.com>
2017-05-02 22:19:31 -0700

    json
    son

---
+++ json.js
@@ -0,0 +1,124 @@
+{"employees":[
+    {
+        "id": "1001",
+        "name": "amy jones",
+        "title": "ceo",
+        "imagePath": "directory/images/amy_jones.jpg",
+        "officeNumber": "858-111-1111",
+        "cellNumber": "619-111-1111",
+        "email": "amy.jones@gmail.com",
+        "reportsTo": ""
+        
+    },
+    {
+     "id": "1002",
+        "name": "eugene lee",
+        "title": "ceo",
+        "imagePath": "directory/images/eugene_lee.jpg",
+        "officeNumber": "858-222-2222",
+        "cellNumber": "619-222-2222",
+        "email": "eugene.lee@gmail.com",
+        "reportsTo": ""   
+    },
+    {
+        "id": "1003",
+        "name": "gary donovan",
+        "title": "ceo",
+        "imagePath": "directory/images/gary_donovan.jpg",
+        "officeNumber": "858-333-3333",
+        "cellNumber": "619-333-3333",
+        "email": "gary.donovan@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1004",
+        "name": "james king",
+        "title": "ceo",
+        "imagePath": "directory/images/james_king.jpg",
+        "officeNumber": "858-444-4444",
+        "cellNumber": "619-444-4444",
+        "email": "james.king@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1005",
+        "name": "john williams",
+        "title": "ceo",
+        "imagePath": "directory/images/john_williams.jpg",
+        "officeNumber": "858-555-5555",
+        "cellNumber": "619-555-5555",
+        "email": "john.williams@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1006",
+        "name": "julie taylor",
+        "title": "ceo",
+        "imagePath": "directory/images/julie_taylor.jpg",
+        "officeNumber": "858-666-6666",
+        "cellNumber": "619-666-6666",
+        "email": "julie.taylor@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1007",
+        "name": "kathleen byrne",
+        "title": "ceo",
+        "imagePath": "directory/images/kathleen_byrne.jpg",
+        "officeNumber": "858-777-7777",
+        "cellNumber": "619-777-7777",
+        "email": "kathleen.byrne@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1008",
+        "name": "lisa wong",
+        "title": "ceo",
+        "imagePath": "directory/images/lisa_wong.jpg",
+        "officeNumber": "858-888-8888",
+        "cellNumber": "619-888-8888",
+        "email": "lisa.wong@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1009",
+        "name": "paul jones",
+        "title": "ceo",
+        "imagePath": "directory/images/paul_jones.jpg",
+        "officeNumber": "858-999-9999",
+        "cellNumber": "619-999-9999",
+        "email": "paul.jones@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1010",
+        "name": "paula gates",
+        "title": "ceo",
+        "imagePath": "directory/images/paula_gates.jpg",
+        "officeNumber": "858-101-1010",
+        "cellNumber": "619-101-1010",
+        "email": "paula.gates@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1011",
+        "name": "ray moore",
+        "title": "ceo",
+        "imagePath": "directory/images/ray_moore.jpg",
+        "officeNumber": "858-011-0110",
+        "cellNumber": "619-011-0110",
+        "email": "ray.moore@gmail.com",
+        "reportsTo": ""
+    },
+    {
+        "id": "1012",
+        "name": "steven wells",
+        "title": "ceo",
+        "imagePath": "directory/images/steven_wells.jpg",
+        "officeNumber": "858-012-0120",
+        "cellNumber": "619-012-0120",
+        "email": "steven.wells@gmail.com",
+        "reportsTo": ""
+    }
+   
+]}
